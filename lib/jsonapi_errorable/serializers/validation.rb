b0ddb405b4305c350a4ea4e34488e1093d9fdd72
Daniel Sullivan <daniel.sullivan@reticulum.it>
2018-08-21 09:51:20 +0900

    Missing attibutes field

--- lib/jsonapi_errorable/serializers/validation.rb
+++ lib/jsonapi_errorable/serializers/validation.rb
@@ -38,9 +38,7 @@ module JsonapiErrorable
 
       def each_error
         object.errors.messages.each_pair do |attribute, messages|
-          details = if Rails::VERSION::MAJOR >= 5
-                      object.errors.details.find { |k,v| k == attribute }[1]
-                    end
+          details = object.errors.details.find {|k, v| k == attribute}[1] if Rails::VERSION::MAJOR >= 5 && object.errors.details.find {|k, v| k == attribute}
 
           messages.each_with_index do |message, index|
             code = details[index][:error] if details
